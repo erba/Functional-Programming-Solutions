6c8011c3b49e3aa3dd9c8166b0ad83d2e748e7ef
Ervis Barjaktari <ervis.barjaktari@stud.uni-bamberg.de>
2020-02-21 11:47:12 +0100

    Exercise Sheet 3 without 3.3

--- IF_Solutions.hs
+++ IF_Solutions.hs
@@ -16,11 +16,8 @@ scalarSimple :: Num a => [a] -> [a] -> a
 scalarSimple ls1 ls2 =  sum [a*b | (a,b)<-(zip ls1 ls2)]
 
 -------------------
--- Exercise Sheet 1
+-- EXERCISE SHEET 1
 -------------------
----------------
--- EXERCISE 1.1
----------------
 -- 1.1.1
 double :: Integer -> Integer
 double x = 2 * x
@@ -80,9 +77,6 @@ middle (x:"") = Just x
 -- middle str = Just (str !! (floor ((fromIntegral(length str)) / 2)))
 middle str = Just $ str !! (floor $ (fromIntegral $ length str) / 2)
 
----------------
--- EXERCISE 1.2
----------------
 -- 1.2.1
 -- func :: [a] -> Int
 --
@@ -93,11 +87,8 @@ middle str = Just $ str !! (floor $ (fromIntegral $ length str) / 2)
 -- func :: String -> String -> Bool    works but
 -- func :: [a] -> [a] -> Bool     is the better solution
 
----------------
--- EXERCISE 1.3
----------------
 -- 1.3.1
-natural :: Integer -> Integer
+natural :: Int -> Int
 natural n | n <= 1 = 0
           | n > 1 = 1 + natural (n-1)
 
@@ -113,13 +104,13 @@ sumRec n | n <= 0 = 0
          | n > 0 = n + sumRec (n-1)
 
 -- 1.3.4
-fact :: Integer -> Integer
+fact :: Int -> Int
 fact n  | n == 0 = 1
         | n > 0 = n * fact (n-1)
         | n < 0 = n * fact (n+1)
 
 -- 1.3.5
-sumFact :: Integer -> Integer
+sumFact :: Int -> Int
 sumFact n | n == 0 = fact 0
           | otherwise = (fact n) + sumFact (n-1)
 
@@ -166,9 +157,6 @@ intSqrt n  = intSqrt' 0 n
 
 -- 1.3.12 - varja
 
----------------
--- EXERCISE 1.4
----------------
 -- 1.4.1
 fibo :: Int -> Int
 fibo 0 = 0
@@ -184,23 +172,491 @@ fiboTwo n = step $ fiboTwo (n-1) where
 superFibo :: Int -> Int
 superFibo n = fst twoPreviousFibo + snd twoPreviousFibo
     where twoPreviousFibo = fiboTwo (n-2)
-
 ------------------------------------------------------------------
--- @ operator usage: 
+-- @ operator usage:
 -- lst @ (h:tl)
 -- giving the the list, its head and its tail a name
 -- by pattern matching
 ------------------------------------------------------------------
+-------------------
+-- EXERCISE SHEET 2
+-------------------
+-- 2.1.1
+listEvens :: Int -> Int -> [Int]
+listEvens x y | x > y = []
+listEvens x y | otherwise = [k | k <- [x, (x+1) ..  y], even k]
 
-myFunc n m = n ++ m
 
-myFuncAdd :: Num a => a -> a -> a
-myFuncAdd n m = n + m
+-- 2.1.2
+type Triple = (Int, Int, Int)
+--
+pythagoreanTriples :: Int -> [Triple]
+pythagoreanTriples n | n <= 0 = []      -- error prevention
+                     | otherwise = [(a, b, c) | a <- smallerEqualN, b <- smallerEqualN, c <- smallerEqualN, a^2 + b^2 == c^2]
+                         where smallerEqualN = [k | k<-[0 .. n]]
 
-myFuncDiv :: (Fractional a) => a -> Integer -> a
-myFuncDiv n m = n + m
--- 2 `div` 3
+-- 2.1.3
+addPairwise :: [Int] -> [Int] -> [Int]
+addPairwise a b = zipWith (+) a b
 
--- func :: MyTypeClass a => a -> a
---  + / * ++
-arit
+-- 2.1.4
+subList :: [a] -> (Int, Int) -> [a]
+subList [] _       = []
+subList lst (i, j) = [lst!!index | index <- [i .. j]]
+--
+subList' :: [a] -> (Int, Int) -> [a]
+subList' [] _       = []
+subList' lst (i, j) = drop i $ take (j+1) lst
+
+-- 2.1.5
+together :: [a] -> [(a,a)]
+together []     = []
+together (x:[]) = []
+together lst    = [ ( lst!!index, lst!!(index+1) ) | index <- [0 .. lastIndex-1] ]
+                where lastIndex = (length lst) - 1
+
+-- 2.2.1
+contains :: Eq a => [a] -> a -> Bool
+contains [] _ = False
+contains (head:tail) element = element == head || contains tail element
+--
+contains' :: Eq a => [a] -> a -> Bool
+contains' lst element = any (== element) lst
+
+-- 2.2.2
+nth :: Int -> [a] -> Maybe a
+nth out lst   | out < 0 || out > ( (length lst) - 1) = Nothing    -- not really necessary, since thiese csaes are handled by the following lines; but for the sake of efficiency
+nth _ []      = Nothing
+nth 0 (x: xs) = Just x
+nth n (x: xs) = nth (n-1) xs
+
+-- 2.2.3
+remove :: Eq a => a -> [a] -> [a]
+remove _ [] = []
+remove i (x:xs) | i == x = remove i xs
+                | i /= x = x : (remove i xs)
+--
+remove' :: Eq a => a -> [a] -> [a]
+remove' i lst = filter (/= i) lst
+
+-- 2.2.4
+substitute :: Eq a => a -> a -> [a] -> [a]
+substitute _ _ [] = []
+substitute a b (x:xs) | x == a = b : (substitute a b xs)
+                      | x /= a = x : (substitute a b xs)
+--
+substitute' :: Eq a => a -> a -> [a] -> [a]
+substitute' a b lst = map (aToB) lst
+    where aToB x | x == a = b
+                 | otherwise = x
+
+-- 2.3.1
+addMinutes :: [(Int, Int)] -> (Int, Int)
+addMinutes [] = (0, 0)
+addMinutes (h:tail) = addTwoTimes h (addMinutes tail)
+    where addTwoTimes (m1,s1) (m2,s2) = (m1 + m2 + (s1 + s2) `div` 60, (s1+s2) `mod` 60)
+
+-- 2.3.2
+type Moves = (Char, Char)
+type Solution = [Moves]
+hanoi :: Int -> Solution
+hanoi n = hanoi' n 'a' 'c'
+--
+hanoi' :: Int -> Char -> Char -> Solution
+hanoi' 0 _ _ = []    -- error prevention
+hanoi' _ startPole endPole | startPole == endPole = []
+hanoi' _ startPole _ | startPole /= 'a' && startPole /= 'b' && startPole /= 'c' = [('E', 'R')]    -- error
+hanoi' _ _ endPole | endPole /= 'a' && endPole /= 'b' && endPole /= 'c' = [('E', 'R')]    -- error
+--
+hanoi' 1 startPole endPole = [ (startPole, endPole) ]
+--
+hanoi' 2 startPole endPole = hanoi' 1 startPole freePole ++ hanoi' 1 startPole endPole ++ hanoi' 1 freePole endPole
+    where freePole = getFreePole startPole endPole
+--
+hanoi' 3 startPole endPole = hanoi' 2 startPole freePole ++ hanoi' 1 startPole endPole ++ hanoi' 2 freePole endPole
+    where freePole = getFreePole startPole endPole
+--
+hanoi' n startPole endPole = hanoi' (n-1) startPole freePole ++ hanoi' 1 startPole endPole ++ hanoi' (n-1) freePole endPole
+    where freePole = getFreePole startPole endPole
+--
+getFreePole :: Char -> Char -> Char
+getFreePole 'a' 'b' = 'c'
+getFreePole 'a' 'c' = 'b'
+getFreePole 'b' 'a' = 'c'
+getFreePole 'b' 'c' = 'a'
+getFreePole 'c' 'a' = 'b'
+getFreePole 'c' 'b' = 'a'
+
+-- 2.4.1
+addDashes :: [String] -> [String]
+addDashes lst = map (\x -> "-- " ++ x) lst
+
+-- 2.4.2
+swapPairs :: [(a,b)] -> [(b, a)]
+swapPairs lst = map (\(x, y) -> (y, x)) lst
+
+-- 2.4.3
+applyEach :: [((a -> b), a)] -> [b]
+applyEach lst = map (\(f, x) -> f x) lst
+--
+applyEach' :: [((a -> b), a)] -> [b]
+applyEach' lst = [f y | (f,y) <- lst]
+
+-- 2.4.4
+theSame :: String -> Bool
+theSame "" = True
+theSame str@(x:_) = foldr1 (&&) (map ( == x) str)
+
+-- 2.5.1
+twice :: (a -> a) -> a -> a
+twice f x = (f . f) x
+-- twice f x = f (f x)
+-- twice f x = f $ f x
+
+-- 2.5.2
+maxRef :: (Int -> Int) -> Int -> Int
+maxRef f n | n < 0 = 0
+           | n >= 0 =  max (f n) (maxRef f (n - 1))
+
+-- 2.5.3
+oneZero :: (Int -> Int) -> Int -> Bool
+oneZero f n = any (== 0) [f x | x <- [0 .. n]]
+
+-- 2.5.4
+iter :: Int -> (a -> a) -> a -> a
+iter 1 f x = f x
+iter n f x = f $ iter (n-1) f x
+--
+iter' :: Int -> (a -> a) -> a -> a
+iter' n f x = foldr ($) (f x) (replicate (n-1) f)
+
+-- 2.5.5
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny _ [] = False
+myAny f (h:tail) = f h || myAny f tail
+--
+myAny' :: (a -> Bool) -> [a] -> Bool
+myAny' f lst = foldr (||) False [f x | x <- lst]
+----
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll _ [] = True
+myAll f (h:tail) = f h && myAll f tail
+--
+myAll' :: (a -> Bool) -> [a] -> Bool
+myAll' f lst = foldr (&&) True [f x | x <- lst]
+----
+mySum :: Num a => [a] -> a
+mySum [] = 0
+mySum (h:tail) = h + mySum tail
+--
+mySum' :: Num a => [a] -> a
+mySum' lst = foldr (+) 0 lst
+
+-- 2.5.6
+myUnzip :: [(a, b)] -> ([a], [b])
+myUnzip lst = foldr (\x y -> (fst x : fst y, snd x : snd y)) ([], []) lst
+
+-- 2.5.7
+myMap :: (a -> b) -> [a] -> [b]
+myMap f lst = [f k | k <- lst]
+
+-- 2.5.8
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter f (h:tail) = if f h == True
+                         then h : myFilter f tail
+                      else myFilter f tail
+
+-- 2.5.9
+natInd :: a -> (Int -> a -> a) -> Int -> a
+natInd def f 0 = def
+-- natInd def f n = f (natInd def f (n-1))
+-- X0 = 0
+-- Xn+1 = Xn + 1
+-- Xn+1 = step_function(Xn)
+-------------------
+-- EXERCISE SHEET 3
+-------------------
+
+-- 3.1.1
+data Season = Spring | Summer | Autumn | Winter
+    deriving (Eq, Show, Enum)
+theSeasons :: [Season]
+theSeasons = enumFrom Spring
+
+-- 3.1.2
+seasonsFrom :: Season -> [Season]
+seasonsFrom n = enumFrom n
+--
+seasonsFrom' :: Season -> [Season]
+seasonsFrom' n = [n .. Winter]
+
+-- 3.1.3
+mapSeasonsFrom :: [Season] -> [ [Season] ]
+mapSeasonsFrom n = [seasonsFrom k | k <- n]
+
+-- 3.1.4
+data Month = Jan | Feb | Mar | Apr | May | Jun | Jul | Aug | Sep | Oct | Nov | Dec
+    deriving (Eq, Ord, Show, Enum)
+-- fromEnum Feb
+-- toEnum 2::Motth
+
+-- 3.1.5
+monthFromTo :: Month -> Month -> [Month]
+monthFromTo f t = [f .. t]
+
+-- 3.1.6
+monthToSeason :: Month -> Season
+monthToSeason n | elem n ([Dec] ++ [Jan .. Feb]) = Winter
+                | elem n [Mar .. May] = Spring
+                | elem n [Jun .. Aug] = Summer
+                | elem n [Sep .. Nov] = Autumn
+-- monthToSeason Dec = Winter
+-- monthToSeason Jan = Winter
+-- monthToSeason Feb = Winter
+-- monthToSeason Mar = Spring
+-- monthToSeason Apr = Spring
+-- monthToSeason May = Spring
+-- monthToSeason Jun = Summer
+-- monthToSeason Jul = Summer
+-- monthToSeason Aug = Summer
+-- monthToSeason Sep = Autumn
+-- monthToSeason Oct = Autumn
+-- monthToSeason Nov = Autumn
+
+-- 3.1.7
+data MyBoolean = MyFalse | MyTrue
+    deriving Show
+boolToMyBoolean :: Bool -> MyBoolean
+boolToMyBoolean True = MyTrue
+boolToMyBoolean False = MyFalse
+--
+myBooleanToBool :: MyBoolean -> Bool
+myBooleanToBool MyFalse = False
+myBooleanToBool MyTrue = True
+
+-- 3.1.8
+(&:&) :: MyBoolean -> MyBoolean -> MyBoolean
+(&:&) MyTrue MyTrue = MyTrue
+(&:&) _ _ = MyFalse
+--
+(|:|) :: MyBoolean -> MyBoolean -> MyBoolean
+(|:|) MyTrue _ = MyTrue
+(|:|) _ MyTrue = MyTrue
+(|:|) _ _ = MyFalse
+
+-- 3.1.9
+myOr :: [MyBoolean] -> MyBoolean
+myOr [] = MyFalse
+myOr (h:tail) = h |:| myOr tail
+--
+myAnd :: [MyBoolean] -> MyBoolean
+myAnd [] = MyTrue
+myAnd (h:tail) = h &:& myAnd tail
+--
+myOr' :: [MyBoolean] -> MyBoolean
+myOr' lst = boolToMyBoolean $ or $ map (myBooleanToBool) lst
+--
+myAnd' :: [MyBoolean] -> MyBoolean
+myAnd' lst = boolToMyBoolean $ and $ map (myBooleanToBool) lst
+--
+myOr'' :: [MyBoolean] -> MyBoolean
+myOr'' lst = foldr (|:|) MyFalse lst
+--
+myAnd'' :: [MyBoolean] -> MyBoolean
+myAnd'' lst = foldr (&:&) MyTrue lst
+
+-- 3.2.1
+data Bit = I | O | Bit Bit deriving (Eq, Show)
+bitsToInt :: [Bit] -> Int
+bitsToInt lst = bitsToIntRec lst 0
+    where bitsToIntRec :: [Bit] -> Int -> Int
+          bitsToIntRec ls index | index == length ls - 1 = if ls!!index == I then 2^(length ls - 1 - index) else 0
+          bitsToIntRec ls index | ls!!index == I = 2^(length ls - 1 - index) + bitsToIntRec ls (index + 1)
+          bitsToIntRec ls index | ls!!index == O = bitsToIntRec ls (index + 1)
+--
+bitsToInt' :: [Bit] -> Int
+bitsToInt' [] = 0
+bitsToInt' lst@(h:t) | h == I = 2^(length lst - 1) + bitsToInt' t
+                     | h == O = bitsToInt' t
+--
+bitsToInt'' :: [Bit] -> Int
+bitsToInt'' lst = snd (foldr (sumIOs) (O, 0) (zip [bit | bit <- lst] [power | power <- [length lst - 1, length lst - 2 .. 0]] ))
+    where sumIOs :: (Bit, Int) -> (Bit, Int) -> (Bit, Int)
+          sumIOs (bit1, power1) (_, sum) = (O, bitTo01 bit1 * 2^power1 + sum)
+          bitTo01 O = 0
+          bitTo01 I = 1
+
+-- 3.2.2
+data Number = Exact Int | Approx Float
+rounded :: Number -> Int
+rounded (Exact x) = x
+rounded (Approx x) = round x
+
+-- 3.4.1
+data Age = Years Int deriving Show
+data Name = Name String String deriving Show
+data Person = Person Name Age deriving Show
+howOld :: Person -> Age
+howOld (Person _ age) = age
+--
+addAges :: Person -> Person -> Age
+addAges (Person _ (Years x)) (Person _ (Years y)) = Years (x+y)
+
+-- 3.4.2
+-- -- to run this paragraph comment out the next two
+-- data Expr = Lit Int | Add Expr Expr | Sub Expr Expr
+-- size :: Expr -> Int
+-- size (Lit _) = 0
+-- size (Add e1 e2) = 1 + (size e1) + (size e2)
+-- size (Sub e1 e2) = 1 + (size e1) + (size e2)
+
+-- 3.4.3
+-- -- to run this paragraph comment out the next and previous one
+-- data Expr = Lit Int | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr
+-- --
+-- eval :: Expr -> Int
+-- eval (Lit n) = n
+-- eval (Add e1 e2) = (eval e1) + (eval e2)
+-- eval (Sub e1 e2) = (eval e1) - (eval e2)
+-- eval (Mul e1 e2) = (eval e1) * (eval e2)
+-- eval (Div e1 e2) = (eval e1) `div` (eval e2)
+-- --
+-- size :: Expr -> Int
+-- size (Lit _) = 0
+-- size (Add e1 e2) = 1 + (size e1) + (size e2)
+-- size (Sub e1 e2) = 1 + (size e1) + (size e2)
+-- size (Mul e1 e2) = 1 + (size e1) + (size e2)
+-- size (Div e1 e2) = 1 + (size e1) + (size e2)
+
+-- 3.5.1
+-- to run this paragraph comment out the previous two
+data Expr = Lit Int | OpExp Op Expr Expr deriving Eq
+data Op = Add | Sub | Mul | Div | Mod deriving Eq
+eval :: Expr -> Int
+eval (Lit n) = n
+eval (OpExp Add e1 e2) = (eval e1) + (eval e2)
+eval (OpExp Sub e1 e2) = (eval e1) - (eval e2)
+eval (OpExp Mul e1 e2) = (eval e1) * (eval e2)
+eval (OpExp Div e1 e2) = (eval e1) `div` (eval e2)
+eval (OpExp Mod e1 e2) = (eval e1) `mod` (eval e2)
+--
+size :: Expr -> Int
+size (Lit _) = 0
+size (OpExp Add e1 e2) = 1 + (size e1) + (size e2)
+size (OpExp Sub e1 e2) = 1 + (size e1) + (size e2)
+size (OpExp Mul e1 e2) = 1 + (size e1) + (size e2)
+size (OpExp Div e1 e2) = 1 + (size e1) + (size e2)
+size (OpExp Mod e1 e2) = 1 + (size e1) + (size e2)
+
+-- 3.5.2
+data IExpr = ILit Int | IADD IExpr IExpr | ISUB IExpr IExpr | IMUL IExpr IExpr | IMOD IExpr IExpr | IF BExpr IExpr IExpr
+data BExpr = BLit Bool | AND BExpr BExpr | OR BExpr BExpr | NOT BExpr | EQUAL IExpr IExpr
+iEval :: IExpr -> Int
+iEval (ILit x) = x
+iEval (IADD e1 e2) = (iEval e1) + (iEval e2)
+iEval (ISUB e1 e2) = (iEval e1) - (iEval e2)
+iEval (IMUL e1 e2) = (iEval e1) * (iEval e2)
+iEval (IMOD e1 e2) = (iEval e1) `mod` (iEval e2)
+iEval (IF be e1 e2) | bEval be == True = iEval e1
+                    | bEval be == False = iEval e2
+--
+bEval :: BExpr -> Bool
+bEval (BLit b) = b
+bEval (AND e1 e2) = (bEval e1) && (bEval e2)
+bEval (OR e1 e2)  = (bEval e1) || (bEval e2)
+bEval (NOT e1)    = not $ bEval e1
+bEval (EQUAL e1 e2)  = (iEval e1) == (iEval e2)
+
+-- 3.6.1
+data Pair a = Pair a a
+    deriving Show    -- derive from Show to print to console
+swapPair :: Pair a -> Pair a
+swapPair (Pair a b) = Pair b a
+--
+eqPair :: Eq a => Pair a -> Bool
+eqPair (Pair a b) = (a == b)
+
+-- 3.6.2
+data List a = EmptyList | Cons a (List a)
+    deriving (Eq, Ord, Show, Read)
+isEmpty :: List a -> Bool
+isEmpty EmptyList = True
+isEmpty _ = False
+--
+lengthOfList :: List a -> Int
+lengthOfList EmptyList = 0
+lengthOfList (Cons a list) = 1 + lengthOfList list
+
+-- 3.7.1
+data Tree a = Empty | Node a (Tree a) (Tree a) | Leaf a
+    deriving Show
+howMany :: Tree a -> Int
+howMany Empty                    = 0
+howMany (Leaf a)                 = 1
+howMany (Node a branchL branchR) = 1 + (howMany branchL) + (howMany branchR)
+
+-- 3.7.2
+depth :: Tree a -> Int
+depth Empty                    = 0
+depth (Leaf a)                 = 1
+depth (Node a branchL branchR) = max (1 + depth branchL) (1 + depth branchR)
+-- shortest path in a tree???
+
+-- 3.7.3
+reflect :: Tree a -> Tree a
+reflect Empty                    = Empty
+reflect (Leaf a)                 = Leaf a
+reflect (Node a branchL branchR) = Node a (reflect branchR) (reflect branchL)
+-- for testing: ( Node 'a' (Node 'b' (Node 'd' (Leaf 'h') (Leaf 'i')) (Node 'e' (Leaf 'j') (Leaf 'k'))) (Node 'c' (Node 'f' (Leaf 'l') (Leaf 'm')) (Node 'g' (Leaf 'n') (Leaf 'o'))) )
+
+-- 3.7.4
+traversal :: Tree a -> [a]
+traversal Empty        = []
+traversal (Leaf x)     = [x]
+traversal (Node x l r) = traversal l ++ [x] ++ traversal r
+--
+post :: Tree a -> [a]
+post Empty                    = []
+post (Leaf x)                 = [x]
+post (Node x branchL branchR) = post branchL ++ post branchR ++ [x]
+--
+pre :: Tree a -> [a]
+pre Empty                    = []
+pre (Leaf x)                 = [x]
+pre (Node x branchL branchR) = [x] ++ pre branchL ++ pre branchR
+
+-- 3.7.5
+normalise :: Tree a -> Tree a
+normalise Empty                    = Empty
+normalise (Leaf x)                 = Leaf x
+normalise (Node x Empty Empty)     = Leaf x
+normalise (Node x branchL branchR) = Node x (normalise branchL) (normalise branchR)
+
+-- 3.7.6
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f Empty                    = Empty
+mapTree f (Leaf x)                 = Leaf (f x)
+mapTree f (Node x branchL branchR) = Node (f x) (mapTree f branchL) (mapTree f branchR)
+--
+natural'' :: Int -> Bool    -- already natural and natural' defined above
+natural'' n = (n >= 0)
+--
+plusOne :: Int -> Int
+plusOne x = x + 1
+
+-- 3.7.7
+listToTree :: Eq a => [a] -> [a] -> Tree a
+listToTree [] [] = Empty
+listToTree [x] [y] = Leaf x
+-- listToTree (y:x:[]) (x':y':[]) = Node x (Leaf y)
+
+-- 3.8.1
+data GenericTree a = GTEmpty | GTLeaf a | GTNode a [GenericTree a] deriving Show    -- Show for console printing
+
+-- 3.8.2
+countGTLeaves :: GenericTree a -> Integer
+countGTLeaves GTEmpty = 0
+countGTLeaves (GTLeaf a) = 1
+countGTLeaves (GTNode a branchList) = sum (map countGTLeaves branchList)
+-- for testing: GTNode 'a' [ (GTNode 'b' [GTNode 'd' [GTNode 'h' [GTNode 'e' [GTLeaf 'n', GTLeaf 'o'], GTLeaf 'm'], GTLeaf 'i'] , GTLeaf 'e']) , GTNode 'c' [GTNode 'f' [GTLeaf 'j'], GTNode 'g' [GTLeaf 'k']]]
